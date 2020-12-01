6511211718b00ceebd046aa428a99b5fde875221
UO258418 <UO258418@uniovi.es>
2020-12-02 00:32:04 +0100

    1

---
+++ Cabecera.js
@@ -0,0 +1,9 @@
+var asignatura = {
+    nombre: "Software y estándares en la Web",
+    titulacion: "Grado en Ingeniería Informática del Software",
+    centro: "Escuela de Ingeniería Informática",
+    universidad: "Universidad de Oviedo",
+    curso: "2020-21",
+    estudiante: "Jonás Martínez Cuesta",
+    email: "UO258418@uniovi.es",
+}
